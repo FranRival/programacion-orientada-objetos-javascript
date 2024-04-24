a9cb7f82c31ddbd7fbff571ca3f5725f150b1bce
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-23 22:10:44 -0600

    definicion de POO

---
+++ js/java.js
@@ -0,0 +1,15 @@
+//objetos vs objetos vs objetos.
+
+//crear objetos desde funciones. 
+//instancias de prototipos.
+//crear instancias de prototiopos necesitmoas crear prototipos.
+
+//sintaxis de prototipos; sintaxis de clases. 
+//prototipos: 
+
+
+// NUEVOS OBJETOS > OBJETOS > PROTOTIPOS
+
+//1. pilares de POO
+//2. Objetos a profundidad
+//3. SOlid.
