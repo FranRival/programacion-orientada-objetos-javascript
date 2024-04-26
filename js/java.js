f6a619b49e4c427c0e7d4405d3bd36f91210c622
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-25 21:29:07 -0600

    hay un kistake en el code

--- js/java.js
+++ js/java.js
@@ -1,40 +1,24 @@
 
-//objetos literales y prototipos 
 
-const natalia = { //ya tiene el _proto_
 
-    name: 'Natalia',
-    age: 20,
-    cursosAprovados: [
-        'curso1',
-        'Curso2'
-    ],
-    aprovarCursos(nuevoCurso){//metodos
-        this.cursosAprovados.push(nuevoCurso)
+
+//clases en java
+//clases: sintaxis. por dentro son prototipos. 
+
+class Student {
+    constructor(name, age, cursosAprovados){
+        this.name = name
+        this.age = age
+        this.cursosAprovados=cursosAprovados
+    }
+    aprovarCurso(cursillo){
+        this.cursosAprovados.push(cursillo)
     }
 }
 
-natalia.aprovarCursos.push("sdfdv") /////////NO GOT UNDERSTAND THIS ERROR.
-
-
-
-function Student (name, age, cursosAprovados){
-    //aqui usamos los datos de arriba.
-    this.name = name
-    this.age = age
-    this.cursosAprovados = cursosAprovados
-
-/*     this.aprovarCurso = function (cursillo){
-        this.cursosAprovados.push(cursillo)
-    } */
-
-}
-
-
-//metodo por fuera de la funcion
-Student.prototype.aprovarCurso = function (cursillo){
-    this.cursosAprovados.push(cursillo)
-}
-
-//crear una instancia.
-const ponny = new Student('Pony', 45,['Curso43', 'Curso321'])
+const fran = new Student(
+    'Fran',
+    29,
+    'Curso de ciencias',
+    'CUrso de tontera y media'
+) 
