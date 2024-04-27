e7d5d8dbb5c7db63aebe8e38aeeaf8a0028e242c
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-26 21:53:47 -0600

    ahorrando de escribir tanto this. utilizando el .social media y los corchetes

--- js/java.js
+++ js/java.js
@@ -1,68 +1,26 @@
-const alumno1 = {
-    nombre: 'Persona',
-    username: 'Pers',
-    points: 100,
-    socialMedia: {
-        twitter: 'twitter',
-        facebook: 'facebook',
-        instagram: undefined
-    },
-    cursosAprovadosALV: [
-        'Curso 1',
-        'Curso 2',
-        'Curso 3'
-    ],
-    loqueToca: [
-        {
-            name: 'Escuela desarrllo',
-            courses: [
-                'Curso 1',
-                'Curso 2',
-                'Curso responsivo alv'
-            ]
-        },
-        {
-            name: 'Escuela de videojuegos',
-            courses: [
-                'Curso introduccion',
-                'Curso unreal engine',
-                'Curso unity 3d'
-            ]
-        },
-    ]
-}
+//crear instancias de objetos a la dickenson.
 
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        instagram = undefined,
+        facebook = undefined,
+        cursosAprovados = [],
+        loqueToca =[]
+    }){
+        this.name = name 
+        this.emai = email 
+        this.username = username 
 
-const alumno2 = {
-    nombre: 'Persona2',
-    username: 'Pers2',
-    points: 1000,
-    socialMedia: {
-        twitter: undefined,
-        facebook: 'facebook',
-        instagram: undefined
-    },
-    cursosAprovadosALV: [
-        'Curso data vid',
-        'Curso ciencia de datos',
-        'Curso data inteligence'
-    ],
-    loqueToca: [
-        {
-            name: 'Escuela desarrllo',
-            courses: [
-                'Curso 1',
-                'Curso 2',
-                'Curso responsivo alv'
-            ]
-        },
-        {
-            name: 'Escuela CIencia datos',
-            courses: [
-                'Curso tableu',
-                'Curso dataViz',
-                'Curso dataBussineces.'
-            ]
-        },
-    ]
+        this.socialMedia = {
+            instagram,
+            facebook
+        }
+
+        this.cursosAprovados = cursosAprovados 
+        this.loqueToca = loqueToca 
+
+    }
 }
