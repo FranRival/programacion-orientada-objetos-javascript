4a8834b124d0de3130f371474adabb6b2df5f80e
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-04-27 22:47:09 -0600

    vete al dickenson. es un error de orden alv. hay que descrubir que modofoko shit.

--- js/java.js
+++ js/java.js
@@ -1,90 +1,4 @@
 
-class Student {
-    constructor({
-        name,
-        email,
-        username,
-        instagram = undefined,
-        facebook = undefined,
-        cursosAprovados = [],
-        loqueToca =[]
-    }){
-        this.name = name 
-        this.emai = email 
-        this.username = username 
-
-        this.socialMedia = {
-            instagram,
-            facebook
-        }
-
-        this.cursosAprovados = cursosAprovados 
-        this.loqueToca = loqueToca 
-    }
-}
-
-const studiante2 = new Student ({
-    name: 'studiante2',
-    email: 'studiante2@estudiante.com',
-    username: 'studiante2',
-    loqueToca: [
-        cursoA,
-        cursoB,
-        cursoC
-    ]
-})
-
-const studiante3 = new Student ({
-    name: 'studiante3',
-    email: 'studiante2@estudiante.com',
-    username: 'studiante3',
-    loqueToca: [
-        cursoB,
-        cursoC
-    ]
-})
-
-
-
-class loqueToca {
-    constructor({
-        curso = [],
-        nombre
-    }){
-        this.curso = curso
-        this.nombre = nombre
-    }
-}
-
-const cursoA = new loqueToca({
-    nombre: 'Escuela desarrollo web',
-    curso: [
-        programacionBasica,
-        HTMLyCSS,
-        practico
-    ]
-})
-const cursoB = new loqueToca({
-    nombre: 'Escuela datos cientificos',
-    curso: [
-        programacionBasica,
-        bussines,
-        VisualizacionDatos
-    ]
-})
-
-
-
-const cursoC = new loqueToca({
-    nombre: 'Escuela videojuegos',
-    curso: [
-        programacionBasica,
-        unity,
-        UnrealEngine
-    ]
-})
-
-
 class Course{
     constructor({
         clases = [],
@@ -95,9 +9,6 @@ class Course{
     }
     get nombrecillo(){ //que manden a llamar este. que envia el nombre.
         return this._nombre
-    }
-    cambiarNombre (nombreNuevo){
-        this._nombre = nombreNuevo
     }
 
     set nombrecillo(nombreNuevo){
@@ -143,5 +54,96 @@ const UnrealEngine = new Course({
 })
 
 
-programacionBasica.nombrecillo 
-programacionBasica.cambiarNombre('ANISKS')
+
+class loqueToca {
+    constructor({
+        curso = [],
+        nombre,
+    }){
+        this.curso = curso
+        this.nombre = nombre
+    }
+}
+
+
+
+const cursoA = new loqueToca({
+    nombre: 'Escuela desarrollo web',
+    curso: [
+        programacionBasica,
+        HTMLyCSS,
+        practico
+    ]
+})
+
+const cursoB = new loqueToca({
+    nombre: 'Escuela datos cientificos',
+    curso: [
+        programacionBasica,
+        bussines,
+        VisualizacionDatos
+    ]
+})
+
+
+
+const cursoC = new loqueToca({
+    nombre: 'Escuela videojuegos',
+    curso: [
+        programacionBasica,
+        unity,
+        UnrealEngine
+    ]
+})
+
+
+
+
+class Student {
+    constructor({
+        name,
+        email,
+        username,
+        instagram = undefined,
+        facebook = undefined,
+        cursosAprovados = [],
+        loqueToca =[]
+    }){
+        this.name = name 
+        this.emai = email 
+        this.username = username 
+
+        this.socialMedia = {
+            instagram,
+            facebook
+        }
+
+        this.cursosAprovados = cursosAprovados 
+        this.loqueToca = loqueToca 
+    }
+}
+
+
+
+const studiante2 = new Student ({
+    name: 'studiante2',
+    email: 'studiante2@estudiante.com',
+    username: 'studiante2',
+    loqueToca: [
+        cursoA,
+        cursoB,
+        cursoC
+    ]
+})
+
+const studiante3 = new Student ({
+    name: 'studiante3',
+    email: 'studiante2@estudiante.com',
+    username: 'studiante3',
+    loqueToca: [
+        cursoB,
+        cursoC
+    ]
+})
+
+
