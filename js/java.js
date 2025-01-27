beb4333229baa435d5651dbe4aea39a5bca052ad
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2025-01-26 18:05:34 -0600

    objetos literales a diferencia de clases.

--- js/java.js
+++ js/java.js
@@ -348,8 +348,29 @@ let arr = mensaje.split(' ')
 alert(arr) */
 
 //creo que comienzo a understand esa thing de setters y getters.
+//con ejemplos
 
 //los getters y los setters convinados con objetos
+
+
+//objetos literales vs clases.
+//OBJETO LITERAL
+/* const persona = {
+    nombre: ["Bob", "Smith"],
+    edad: 32,
+    genero: "masculino",
+    intereses: ["música", "esquí"],
+    bio: function () {
+      alert(
+        `${this.nombre[0]} ${this.nombre[1]} tiene ${this.edad} años. Le gusta ${this.intereses[0]} y ${this.intereses[1]}.`,
+      );
+    },
+    saludo: function () {
+      alert("Hola, Soy " + this.nombre[0] + ". ");
+    },
+  };
+ */
+
 
 
 //constructor: 
